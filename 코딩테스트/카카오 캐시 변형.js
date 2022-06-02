79bc570960218a1b2c59aa25a5e17df6a4b31b8d
dlantjdgkgk <dlantjdgkgk@naver.com>
2022-06-02 18:40:23 +0900

    coding test

---
+++ 코딩테스트/카카오 캐시 변형.js
@@ -0,0 +1,28 @@
+function solution(size, arr) {
+    let answer = Array(size).fill(0);
+    arr.forEach((x) => {
+        let pos = -1;
+        //if문이 성립 하지 않더라도 for문 때문에 5번이 도는데 시간 복잡도 괜찮은건지
+        for (let i = 0; i < size; i++) {
+            if (x === answer[i]) pos = i;
+        }
+
+        //Miss 일때
+        if (pos === -1) {
+            for (let i = size - 1; i >= 1; i--) {
+                answer[i] = answer[i - 1];
+            }
+            //Hit 일때
+        } else {
+            for (let i = pos; i >= 1; i--) {
+                answer[i] = answer[i - 1];
+            }
+        }
+        answer[0] = x;
+    });
+
+    return answer;
+}
+
+let arr = [1, 2, 3, 2, 6, 2, 3, 5, 7];
+console.log(solution(5, arr));
