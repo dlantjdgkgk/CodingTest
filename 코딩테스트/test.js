7915cb9d51001e1b5477c314a22bbb95c5fac94d
dlantjdgkgk <dlantjdgkgk@naver.com>
2022-06-08 17:06:56 +0900

    coding test

--- 코딩테스트/test.js
+++ 코딩테스트/test.js
@@ -1,16 +1,29 @@
-function solution(m, arr) {
+function solution(board, moves) {
     let answer = 0;
-    let lt = 0;
-    let sum = 0;
-    for (let rt = 0; rt < arr.length; rt++) {
-        sum = sum + arr[rt];
-        while (sum > m) {
-            sum = sum - arr[lt++];
+    let stack = [];
+    for (let x of moves) {
+        for (let i = 0; i < board.length; i++) {
+            if (board[i][x - 1] !== 0) {
+                let tmp = board[i][x - 1];
+                board[i][x - 1] = 0;
+                if (tmp === stack[stack.length - 1]) {
+                    stack.pop();
+                    answer += 2;
+                } else stack.push(tmp);
+                break;
+            }
         }
-        if (sum === m) answer++;
     }
     return answer;
 }
 
-let a = [1, 2, 1, 3, 1, 1, 1, 2];
-console.log(solution(5, a));
+let a = [
+    [0, 0, 0, 0, 0],
+    [0, 0, 1, 0, 3],
+    [0, 2, 5, 0, 1],
+    [4, 2, 4, 4, 2],
+    [3, 5, 1, 3, 1],
+];
+
+let b = [1, 5, 3, 5, 1, 2, 1, 4];
+console.log(solution(a, b));
