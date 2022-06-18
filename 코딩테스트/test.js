71dc472fc015a15b42b655f505ff706a44e49e21
dlantjdgkgk <dlantjdgkgk@naver.com>
2022-06-18 23:01:54 +0900

    coding test

--- 코딩테스트/test.js
+++ 코딩테스트/test.js
@@ -1,7 +1,19 @@
-function solutions(s) {
-    return Array.from(new Set(s));
+function solutions(array) {
+    let result = 0;
+    let max = 0;
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] > max) {
+            result++;
+            max = array[i];
+        }
+    }
+    return result;
 }
-console.log(solutions(['good', 'time', 'good', 'time', 'student']));
-
-// Array.from() 메서드는 유사 배열 객체(array-like object)나
-// 반복 가능한 객체(iterable object)를 얕게 복사해 새로운Array 객체를 만듭니다.
+let array = [
+    [2, 1],
+    [3, 1],
+    [3, 2],
+    [1, 2],
+    [3, 3],
+];
+console.log(solutions(array));
--- 코딩테스트/큰 수 출력하기.js
+++ 코딩테스트/큰 수 출력하기.js
@@ -1,12 +1,12 @@
-function solution(answer) {
+function solutions(array) {
     let result = [];
-    result.push(answer[0]);
-    for (let i = 1; i < answer.length; i++) {
-        if (answer[i] > answer[i - 1]) {
-            result.push(answer[i]);
+    result.push(array[0]);
+    for (let i = 1; i < array.length; i++) {
+        if (array[i] > array[i - 1]) {
+            result.push(array[i]);
         }
     }
     return result;
 }
-let answer = [7, 3, 9, 5, 6, 12];
-console.log(solution(answer));
+let array = [7, 3, 9, 5, 6, 12];
+console.log(solutions(array));
