7a3f6ffccaf77bb4b119c783a0b6d41010b4b512
dlantjdgkgk <dlantjdgkgk@naver.com>
2022-06-20 17:20:03 +0900

    coding test

--- 코딩테스트/test.js
+++ 코딩테스트/test.js
@@ -1,19 +1,35 @@
-function solutions(array) {
-    let result = 0;
-    let max = 0;
-    for (let i = 0; i < array.length; i++) {
-        if (array[i] > max) {
-            result++;
-            max = array[i];
+function solution(arr) {
+    let answer = 0;
+    let n = arr.length;
+    let dx = [-1, 0, 1, 0];
+    let dy = [0, 1, 0, -1];
+    for (let i = 0; i < n; i++) {
+        for (let j = 0; j < n; j++) {
+            let flag = 1;
+            for (let k = 0; k < 4; k++) {
+                let nx = i + dx[k];
+                let ny = j + dy[k];
+                if (
+                    nx >= 0 &&
+                    nx < n &&
+                    ny >= 0 &&
+                    ny < n &&
+                    arr[nx][ny] >= arr[i][j]
+                ) {
+                    flag = 0;
+                    break;
+                }
+            }
+            if (flag) answer++;
         }
     }
-    return result;
+    return answer;
 }
-let array = [
-    [2, 1],
-    [3, 1],
-    [3, 2],
-    [1, 2],
-    [3, 3],
+let arr = [
+    [5, 3, 7, 2, 3],
+    [3, 7, 1, 6, 1],
+    [7, 2, 5, 3, 4],
+    [4, 3, 6, 4, 1],
+    [8, 7, 3, 5, 2],
 ];
-console.log(solutions(array));
+console.log(solution(arr));
