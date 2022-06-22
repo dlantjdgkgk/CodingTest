07f680829c2ecf53af3089a426731506bc1f77cf
dlantjdgkgk <dlantjdgkgk@naver.com>
2022-06-22 23:49:57 +0900

    coding test

--- 코딩테스트/test.js
+++ 코딩테스트/test.js
@@ -1,35 +1,29 @@
-function solution(arr) {
+function solution(board, moves) {
     let answer = 0;
-    let n = arr.length;
-    let dx = [-1, 0, 1, 0];
-    let dy = [0, 1, 0, -1];
-    for (let i = 0; i < n; i++) {
-        for (let j = 0; j < n; j++) {
-            let flag = 1;
-            for (let k = 0; k < 4; k++) {
-                let nx = i + dx[k];
-                let ny = j + dy[k];
-                if (
-                    nx >= 0 &&
-                    nx < n &&
-                    ny >= 0 &&
-                    ny < n &&
-                    arr[nx][ny] >= arr[i][j]
-                ) {
-                    flag = 0;
-                    break;
-                }
+    let stack = [];
+    for (let x of moves) {
+        for (let i = 0; i < board.length; i++) {
+            if (board[i][x - 1] !== 0) {
+                let tmp = board[i][x - 1];
+                board[i][x - 1] = 0;
+                if (tmp === stack[stack.length - 1]) {
+                    stack.pop();
+                    answer += 2;
+                } else stack.push(tmp);
+                break;
             }
-            if (flag) answer++;
         }
     }
     return answer;
 }
-let arr = [
-    [5, 3, 7, 2, 3],
-    [3, 7, 1, 6, 1],
-    [7, 2, 5, 3, 4],
-    [4, 3, 6, 4, 1],
-    [8, 7, 3, 5, 2],
+
+let a = [
+    [0, 0, 0, 0, 0],
+    [0, 0, 1, 0, 3],
+    [0, 2, 5, 0, 1],
+    [4, 2, 4, 4, 2],
+    [3, 5, 1, 3, 1],
 ];
-console.log(solution(arr));
+
+let b = [1, 5, 3, 5, 1, 2, 1, 4];
+console.log(solution(a, b));
