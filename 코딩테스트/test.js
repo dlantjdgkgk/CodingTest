055998b793ba8ea6b1a855399f8e8ba768ce199c
dlantjdgkgk <dlantjdgkgk@naver.com>
2022-06-23 17:04:29 +0900

    coding test

--- 코딩테스트/test.js
+++ 코딩테스트/test.js
@@ -1,29 +1,35 @@
-function solution(board, moves) {
+function compareMaps(map1, map2) {
+    if (map1.size !== map2.size) return false;
+    for (let [key, val] of map1) {
+        if (!map2.has(key) || map2.get(key) !== val) return false;
+    }
+    return true;
+}
+function solution(s, t) {
     let answer = 0;
-    let stack = [];
-    for (let x of moves) {
-        for (let i = 0; i < board.length; i++) {
-            if (board[i][x - 1] !== 0) {
-                let tmp = board[i][x - 1];
-                board[i][x - 1] = 0;
-                if (tmp === stack[stack.length - 1]) {
-                    stack.pop();
-                    answer += 2;
-                } else stack.push(tmp);
-                break;
-            }
-        }
+    let tH = new Map();
+    let sH = new Map();
+    for (let x of t) {
+        if (tH.has(x)) tH.set(x, tH.get(x) + 1);
+        else tH.set(x, 1);
+    }
+    let len = t.length - 1;
+    for (let i = 0; i < len; i++) {
+        if (sH.has(s[i])) sH.set(s[i], sH.get(s[i]) + 1);
+        else sH.set(s[i], 1);
+    }
+    let lt = 0;
+    for (let rt = len; rt < s.length; rt++) {
+        if (sH.has(s[rt])) sH.set(s[rt], sH.get(s[rt]) + 1);
+        else sH.set(s[rt], 1);
+        if (compareMaps(sH, tH)) answer++;
+        sH.set(s[lt], sH.get(s[lt]) - 1);
+        if (sH.get(s[lt]) === 0) sH.delete(s[lt]);
+        lt++;
     }
     return answer;
 }
 
-let a = [
-    [0, 0, 0, 0, 0],
-    [0, 0, 1, 0, 3],
-    [0, 2, 5, 0, 1],
-    [4, 2, 4, 4, 2],
-    [3, 5, 1, 3, 1],
-];
-
-let b = [1, 5, 3, 5, 1, 2, 1, 4];
+let a = 'bacaAacba';
+let b = 'abc';
 console.log(solution(a, b));
