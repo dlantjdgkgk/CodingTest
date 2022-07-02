87a453277a8a9a747ef4e81afd5793785f6234a4
dlantjdgkgk <dlantjdgkgk@naver.com>
2022-07-02 23:01:11 +0900

    coding

--- 코딩테스트/test.js
+++ 코딩테스트/test.js
@@ -1,19 +1,36 @@
-function solution(n, a, arr) {
-    let answer;
-    let result = new Set(); // {}
-    for (let i = 0; i < n; i++) {
-        for (let j = i + 1; j < n; j++) {
-            for (let k = j + 1; k < n; k++) {
-                result.add(arr[i] + arr[j] + arr[k]);
-            }
+function solutions(arrOne, arrTwo) {
+    let arr = [];
+    let answer = [];
+    for (let i = 0; i < arrOne.length; i++) {
+        for (let j = 0; j < arrTwo.length; j++) {
+            if (arrOne[i] === arrTwo[j]) arr.push(arrTwo[j]);
         }
     }
-    console.log(result);
-
-    let final_result = Array.from(result).sort((a, b) => b - a);
-    console.log(final_result);
-    answer = final_result[a - 1];
+    answer = arr.sort((a, b) => a - b);
     return answer;
 }
-let arr = [13, 15, 34, 23, 45, 65, 33, 11, 26, 42];
-console.log(solution(10, 3, arr));
+let arrOne = [1, 3, 5, 9, 2];
+let arrTwo = [3, 2, 5, 7, 8];
+console.log(solutions(arrOne, arrTwo));
+// 시간 복잡도 O(nm)
+// 이렇게 구현하였을 때 시간 복잡도가 오래 걸리는지..?
+
+function solution(arr1, arr2) {
+    let answer = [];
+    arr1.sort((a, b) => a - b);
+    arr2.sort((a, b) => a - b);
+    let p1 = (p2 = 0);
+    while (p1 < arr1.length && p2 < arr2.length) {
+        if (arr1[p1] == arr2[p2]) {
+            answer.push(arr1[p1++]);
+            p2++;
+        } else if (arr1[p1] < arr2[p2]) p1++;
+        else p2++;
+    }
+    return answer;
+}
+
+let a = [1, 3, 9, 5, 2]; // 1,2,3,5,9
+let b = [3, 2, 5, 7, 8]; // 2,3,5,7,8
+console.log(solution(a, b));
+// 시간 복잡도 O(n+m)
