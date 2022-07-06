2aa68041f08ed141694c1940202d4e28a68f4bdb
dlantjdgkgk <dlantjdgkgk@naver.com>
2022-07-06 21:28:51 +0900

    coding test

--- 코딩테스트/test.js
+++ 코딩테스트/test.js
@@ -1,16 +1,24 @@
 function solution(arr) {
-    let answer = arr;
-    for (let i = 1; i < arr.length; i++) {
-        let tmp = arr[i];
-        for (let j = i - 1; j <= 0; j--) {
-            if (arr[j] > tmp) {
-                arr[j + 1] = arr[j];
-            } else break;
+    let answer = 0;
+    arr.sort((a, b) => {
+        if (a[1] === b[1]) return a[0] - b[0];
+        else return a[1] - b[1];
+    });
+    let et = 0;
+    for (let x of arr) {
+        if (x[0] >= et) {
+            answer++;
+            et = x[1];
         }
-        arr[j + 1] = tmp;
     }
     return answer;
 }
 
-let arr = [11, 7, 5, 6, 10, 9];
+let arr = [
+    [1, 4],
+    [2, 3],
+    [3, 5],
+    [4, 6],
+    [5, 7],
+];
 console.log(solution(arr));
