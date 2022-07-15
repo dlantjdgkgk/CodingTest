18bd207f375a708b28985394ea55a22a09e9fe40
dlantjdgkgk <dlantjdgkgk@naver.com>
2022-07-15 23:13:00 +0900

    coding test

--- 코딩테스트/test.js
+++ 코딩테스트/test.js
@@ -1,15 +1,16 @@
-function solution(need, plan) {
-    let answer = 'YES';
-    let queue = need.split('');
-    for (let x of plan) {
-        if (queue.includes(x)) {
-            if (x !== queue.shift()) return 'NO';
-        }
+function solutions(arr1, arr2) {
+    let answer = [];
+    let n = arr1.length;
+    let m = arr2.length;
+    let p1 = (p2 = 0);
+    while (p1 < n && p2 < m) {
+        if (arr1[p1] <= arr2[p2]) answer.push(arr1[p1++]);
+        else answer.push(arr2[p2++]);
     }
-    if (queue.length > 0) return 'NO';
+    while (p1 < n) answer.push(arr1[p1++]);
+    while (p2 < m) answer.push(arr2[p2++]);
     return answer;
 }
-
-let a = 'CBA';
-let b = 'CBDAGE';
-console.log(solution(a, b));
+let a = [1, 3, 5];
+let b = [2, 3, 6, 7, 9];
+console.log(solutions(a, b));
