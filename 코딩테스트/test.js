4ead5d91533ce93c1078fe016005e3063f8159d5
dlantjdgkgk <dlantjdgkgk@naver.com>
2022-07-16 23:12:33 +0900

    coding test

--- 코딩테스트/test.js
+++ 코딩테스트/test.js
@@ -1,16 +1,17 @@
-function solutions(arr1, arr2) {
-    let answer = [];
-    let n = arr1.length;
-    let m = arr2.length;
-    let p1 = (p2 = 0);
-    while (p1 < n && p2 < m) {
-        if (arr1[p1] <= arr2[p2]) answer.push(arr1[p1++]);
-        else answer.push(arr2[p2++]);
+function solution(array) {
+    let answer = 'YES';
+    let stack = [];
+    for (let x of array) {
+        if (x === '(') {
+            stack.push(x);
+        } else {
+            if (stack.length === 0) return 'No';
+            stack.pop();
+        }
     }
-    while (p1 < n) answer.push(arr1[p1++]);
-    while (p2 < m) answer.push(arr2[p2++]);
+    if (stack.length > 0) return 'No';
     return answer;
 }
-let a = [1, 3, 5];
-let b = [2, 3, 6, 7, 9];
-console.log(solutions(a, b));
+
+let a = '()()';
+console.log(solution(a));
