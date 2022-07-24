230fbea73168c11f751f0315122d8030b7977d92
dlantjdgkgk <dlantjdgkgk@naver.com>
2022-07-24 22:48:22 +0900

    coding test

--- 코딩테스트/test.js
+++ 코딩테스트/test.js
@@ -1,18 +1,18 @@
-function solutions() {
-    let answer = 'assssdwasd';
-    let result = '';
-    for (let x of answer) {
-        if (result.indexOf(x) === -1) {
-            result = result + x;
-        }
+function solution(str1, str2) {
+    let answer = 'YES';
+    let sH = new Map();
+    for (let x of str1) {
+        if (sH.has(x)) sH.set(x, sH.get(x) + 1);
+        else sH.set(x, 1);
     }
-    return result;
+    for (let x of str2) {
+        if (!sH.has(x) || sH.get(x) === 0) return 'NO';
+        sH.set(x, sH.get(x) - 1);
+    }
+    return answer;
 }
-console.log(solutions());
 
-//2번째 풀었을 때
-const solutions = (arr) => {
-    return new Set(arr);
-};
-let arr = 'ksekkset';
-console.log(solutions(arr));
+
+let a = 'AbaAeCe';
+let b = 'baeeACA';
+console.log(solution(a, b));
