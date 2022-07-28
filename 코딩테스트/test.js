9c0c1ca0b8fb34edb90f219efd61f140b6e92468
dlantjdgkgk <dlantjdgkgk@naver.com>
2022-07-28 21:29:57 +0900

    coding test

--- 코딩테스트/test.js
+++ 코딩테스트/test.js
@@ -1,21 +1,9 @@
 function solutions(arr) {
-    let max = Number.MIN_SAFE_INTEGER;
-    let sum = 0;
-    let answer;
-    for (let x of arr) {
-        let sum = x
-            .toString()
-            .split('')
-            .reduce((a, b) => a + Number(b), 0);
-        if (sum > max) {
-            max = sum;
-            answer = x;
-        } else if (sum === max) {
-            if (x > answer) answer = x;
-        }
-    }
-    return answer;
+    let result = arr.toLowerCase();
+    let answer = result.split('').reverse().join('');
+    if (result === answer) {
+        console.log('YES');
+    } else console.log('NO');
 }
-
-let arr = [128, 460, 603, 40, 146, 521, 137, 123];
+let arr = 'abcba';
 console.log(solutions(arr));
