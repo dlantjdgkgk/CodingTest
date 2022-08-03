130a604b482f2804356f5245b04419fc2ed5502a
dlantjdgkgk <dlantjdgkgk@naver.com>
2022-08-03 23:37:50 +0900

    coding

--- 코딩테스트/test.js
+++ 코딩테스트/test.js
@@ -1,24 +1,13 @@
-function solution(s, t) {
-    let answer = [];
-    let p = 0;
-    for (let x of s) {
-        if (x === t) {
-            p = 0;
-            answer.push(p);
-        } else {
-            p++;
-            answer.push(p);
-        }
-    }
-    for (let i = s.length - 1; i >= 0; i--) {
-        if (s[i] === t) p = 0;
-        else {
-            p++;
-            answer[i] = Math.min(answer[i], p);
+function solutions() {
+    let arr = [87, 87, 87, 100, 76];
+    let answer = [1, 1, 1, 1, 1];
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 0; j < arr.length; j++) {
+            if (arr[i] < arr[j]) {
+                answer[i]++;
+            }
         }
     }
     return answer;
 }
-
-let str = 'teachermade';
-console.log(solution(str, 'e'));
+console.log(solutions());
