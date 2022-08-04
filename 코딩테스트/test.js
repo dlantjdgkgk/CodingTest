7c13572adc06538d7cdff2de8ae093ad39ecd97e
dlantjdgkgk <dlantjdgkgk@naver.com>
2022-08-04 21:51:09 +0900

    coding test

--- 코딩테스트/test.js
+++ 코딩테스트/test.js
@@ -1,13 +1,14 @@
-function solutions() {
-    let arr = [87, 87, 87, 100, 76];
-    let answer = [1, 1, 1, 1, 1];
-    for (let i = 0; i < arr.length; i++) {
-        for (let j = 0; j < arr.length; j++) {
-            if (arr[i] < arr[j]) {
-                answer[i]++;
-            }
+function solution(n, k) {
+    let answer;
+    let queue = Array.from({ length: n }, (v, i) => i + 1);
+    while (queue.length) {
+        for (let i = 1; i < k; i++) {
+            queue.push(queue.shift());
         }
+        queue.shift();
+        if (queue.length === 1) answer = queue.shift();
     }
     return answer;
 }
-console.log(solutions());
+
+console.log(solution(8, 3));
