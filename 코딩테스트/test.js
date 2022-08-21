6943cf931bf26fcf5e34653c530f7d5a05ead5d7
dlantjdgkgk <dlantjdgkgk@naver.com>
2022-08-21 21:05:28 +0900

    test

--- 코딩테스트/test.js
+++ 코딩테스트/test.js
@@ -1,27 +1,15 @@
-function solutions() {
-    let answer = 'study';
-    let final_result = '';
-    if (answer.length % 2 !== 0) {
-        let result = Math.floor(answer.length / 2);
-        final_result += answer[result];
-    } else {
-        let result = answer.length / 2;
-        final_result = answer[result - 1] + answer[result];
+function solution(need, plan) {
+    let answer = 'YES';
+    let queue = need.split('');
+    for (let x of plan) {
+        if (queue.includes(x)) {
+            if (x !== queue.shift()) return 'NO';
+        }
     }
-    return final_result;
+    if (queue.length > 0) return 'NO';
+    return answer;
 }
-console.log(solutions());
 
-// 2번째 풀었을 때
-
-// const solutions = (arr) => {
-//     if (arr.length % 2 === 1) {
-//         let result = arr.splice(arr.length / 2, 1);
-//         return result;
-//     } else {
-//         let result = arr.splice(arr.length / 2 - 1, 2);
-//         return result;
-//     }
-// };
-// let arr = ['s', 'w', 'i', 'm', 'h', 'i', 'j', 'i'];
-// console.log(solutions(arr));
+let a = 'CBA';
+let b = 'CBDAGE';
+console.log(solution(a, b));
