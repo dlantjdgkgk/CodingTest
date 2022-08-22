e804731dcb8ed5bfc8c1d484f1476f5606cddf5c
dlantjdgkgk <dlantjdgkgk@naver.com>
2022-08-22 16:34:20 +0900

    test

--- 코딩테스트/test.js
+++ 코딩테스트/test.js
@@ -1,15 +1,35 @@
-function solution(need, plan) {
-    let answer = 'YES';
-    let queue = need.split('');
-    for (let x of plan) {
-        if (queue.includes(x)) {
-            if (x !== queue.shift()) return 'NO';
+function solution(arr) {
+    let answer = 0;
+    let n = arr.length;
+    let dx = [-1, 0, 1, 0];
+    let dy = [0, 1, 0, -1];
+    for (let i = 0; i < n; i++) {
+        for (let j = 0; j < n; j++) {
+            let flag = 1;
+            for (let k = 0; k < 4; k++) {
+                let nx = i + dx[k];
+                let ny = j + dy[k];
+                if (
+                    nx >= 0 &&
+                    nx < n &&
+                    ny >= 0 &&
+                    ny < n &&
+                    arr[nx][ny] >= arr[i][j]
+                ) {
+                    flag = 0;
+                    break;
+                }
+            }
+            if (flag) answer++;
         }
     }
-    if (queue.length > 0) return 'NO';
     return answer;
 }
-
-let a = 'CBA';
-let b = 'CBDAGE';
-console.log(solution(a, b));
+let arr = [
+    [5, 3, 7, 2, 3],
+    [3, 7, 1, 6, 1],
+    [7, 2, 5, 3, 4],
+    [4, 3, 6, 4, 1],
+    [8, 7, 3, 5, 2],
+];
+console.log(solution(arr));
