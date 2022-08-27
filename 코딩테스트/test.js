9a07b8a57074f7d3a09d5b0cbf0fc02238b815eb
dlantjdgkgk <dlantjdgkgk@naver.com>
2022-08-27 17:57:47 +0900

    코테

--- 코딩테스트/test.js
+++ 코딩테스트/test.js
@@ -1,35 +1,18 @@
-function compareMaps(map1, map2) {
-    if (map1.size !== map2.size) return false;
-    for (let [key, val] of map1) {
-        if (!map2.has(key) || map2.get(key) !== val) return false;
+function isPrime(num) {
+    if (num === 1) return false;
+    for (let i = 2; i <= parseInt(num / 2); i++) {
+        if (num % i === 0) return false;
     }
     return true;
 }
-function solution(s, t) {
-    let answer = 0;
-    let tH = new Map();
-    let sH = new Map();
-    for (let x of t) {
-        if (tH.has(x)) tH.set(x, tH.get(x) + 1);
-        else tH.set(x, 1);
-    }
-    let len = t.length - 1;
-    for (let i = 0; i < len; i++) {
-        if (sH.has(s[i])) sH.set(s[i], sH.get(s[i]) + 1);
-        else sH.set(s[i], 1);
-    }
-    let lt = 0;
-    for (let rt = len; rt < s.length; rt++) {
-        if (sH.has(s[rt])) sH.set(s[rt], sH.get(s[rt]) + 1);
-        else sH.set(s[rt], 1);
-        if (compareMaps(sH, tH)) answer++;
-        sH.set(s[lt], sH.get(s[lt]) - 1);
-        if (sH.get(s[lt]) === 0) sH.delete(s[lt]);
-        lt++;
+
+function solutions(arr) {
+    let answer = [];
+    for (let x of arr) {
+        let result = parseInt(String(x).split('').reverse().join(''));
+        if (isPrime(result)) answer.push(result);
     }
     return answer;
 }
-
-let a = 'bacaAacba';
-let b = 'abc';
-console.log(solution(a, b));
+let arr = [32, 55, 62, 20, 250, 370, 200, 30, 100];
+console.log(solutions(arr));
