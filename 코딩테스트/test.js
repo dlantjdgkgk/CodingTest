e511d14cd81af1a7b32dc5e31155ea820c671813
dlantjdgkgk <dlantjdgkgk@naver.com>
2022-10-27 23:55:52 +0900

    coding test

--- 코딩테스트/test.js
+++ 코딩테스트/test.js
@@ -1,27 +1,9 @@
-function solution(m, product) {
-    let answer = 0;
-    let n = product.length;
-    product.sort((a, b) => a[0] + a[1] - (b[0] + b[1]));
-    for (let i = 0; i < n; i++) {
-        let money = m - (product[i][0] / 2 + product[i][1]);
-        let cnt = 1;
-        for (let j = 0; j < n; j++) {
-            if (j !== i && product[j][0] + product[j][1] > money) break;
-            if (j !== i && product[j][0] + product[j][1] <= money) {
-                money -= product[j][0] + product[j][1];
-                cnt++;
-            }
-        }
-        answer = Math.max(answer, cnt);
-    }
-    return answer;
+function solutions(arr) {
+    let result = arr.toLowerCase().replace(/[^a-z]/g, '');
+    let answer = result.split('').reverse().join('');
+    if (result === answer) {
+        console.log('YES');
+    } else console.log('NO');
 }
-
-let arr = [
-    [6, 6],
-    [2, 2],
-    [4, 3],
-    [4, 5],
-    [10, 3],
-];
-console.log(solution(28, arr));
+let arr = 'found7, time: study; Yduts; emit, 7Dnuof';
+console.log(solutions(arr));
