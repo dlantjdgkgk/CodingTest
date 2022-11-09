a3db841c7e700b14c016391111e699a810c02e9f
dlantjdgkgk <dlantjdgkgk@naver.com>
2022-11-09 20:14:35 +0900

    코테

--- 코딩테스트/test.js
+++ 코딩테스트/test.js
@@ -1,12 +1,18 @@
-function solution(participant, completion) {
-    let answer = '';
-    participant.sort();
-    completion.sort();
-
-    for (let i = 0; i < participant.length; i++) {
-        if (participant[i] !== completion[i]) {
-            answer = answer + participant[i];
-            return answer;
+function solutions(arr) {
+    let max = Number.MIN_SAFE_INTEGER;
+    let sum = 0;
+    let answer;
+    for (let x of arr) {
+        let sum = x
+            .toString()
+            .split('')
+            .reduce((a, b) => a + Number(b), 0);
+        if (sum > max) {
+            max = sum;
+            answer = x;
+        } else if (sum === max) {
+            if (x > answer) answer = x;
         }
     }
+    return answer;
 }
