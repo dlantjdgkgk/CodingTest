7b89cc184c7fdcef29ad2f1d44056bd0dd98c9e7
dlantjdgkgk <dlantjdgkgk@naver.com>
2022-11-25 23:51:17 +0900

    test

--- 코딩테스트/test.js
+++ 코딩테스트/test.js
@@ -1,20 +1,17 @@
-function solution(brown, yellow) {
-    var answer = [];
-    let sum = brown + yellow;
-
-    //카펫의 최소높이는 3부터이다.(위아래 갈색, 가운데가 노란색이기 때문에)
-    for (let height = 3; height <= brown; height++) {
-        //임의의 높이로 나눌때 나머지가 없을경우만
-        if (sum % height === 0) {
-            //가로길이
-            let weight = sum / height;
-
-            //테두리를 제외한 길이를 구해야하기 때문에 각각 -2해준뒤 곱셈
-            //결과가 yellow와 같다면 해당 높이와 길이 리턴
-            if ((height - 2) * (weight - 2) === yellow) {
-                return [weight, height];
-            }
+function solution(array) {
+    let answer = 'yes';
+    let stack = [];
+    for (let x of array) {
+        if (x === '(') {
+            stack.push(x);
+        } else {
+            if (stack.length === 0) return 'no';
+            stack.pop();
         }
     }
+    if (stack.length > 0) return 'no';
     return answer;
 }
+
+let a = '()()';
+console.log(solution(a));
